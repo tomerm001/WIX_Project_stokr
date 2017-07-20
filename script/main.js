1fbf4a8db090b1b331b88e23a1ac695a380f76d5
Tomer Marx <marx.tomer@gmail.com>
2017-07-20 16:43:35 +0300

    add nrmalize css and encapsulate JS

--- script/main.js
+++ script/main.js
@@ -1,47 +1,44 @@
-/**
- * Created by tomerm on 18/07/2017.
- */
-
+(function () {
 
 // DATA
 
-let stockSymbolList = [ "WIX", "MSFT", "YHOO"];
+  let stockSymbolList = ["WIX", "MSFT", "YHOO"];
 
-let stockData = [
-  {
-    "Symbol": "WIX",
-    "Name": "Wix.com Ltd.",
-    "Change": "0.750000",
-    "PercentChange": "+1.51%",
-    "LastTradePriceOnly": "76.099998"
-  },
-  {
-    "Symbol": "MSFT",
-    "Name": "Microsoft Corporation",
-    "PercentChange": "-2.09%",
-    "Change": "-0.850006",
-    "LastTradePriceOnly": "69.620003"
-  },
-  {
-    "Symbol": "YHOO",
-    "Name": "Yahoo! Inc.",
-    "Change": "0.279999",
-    "PercentChange": "+1.11%",
-    "LastTradePriceOnly": "50.599998"
-  }
-];
+  let stockData = [
+    {
+      "Symbol": "WIX",
+      "Name": "Wix.com Ltd.",
+      "Change": "0.750000",
+      "PercentChange": "+1.51%",
+      "LastTradePriceOnly": "76.099998"
+    },
+    {
+      "Symbol": "MSFT",
+      "Name": "Microsoft Corporation",
+      "PercentChange": "-2.09%",
+      "Change": "-0.850006",
+      "LastTradePriceOnly": "69.620003"
+    },
+    {
+      "Symbol": "YHOO",
+      "Name": "Yahoo! Inc.",
+      "Change": "0.279999",
+      "PercentChange": "+1.11%",
+      "LastTradePriceOnly": "50.599998"
+    }
+  ];
 
-let buttonState = "2";
+  let buttonState = "2";
 
 // Clears all HTML content from appContainer
-function clearAppContainer() {
-  let container = document.getElementsByClassName("appContainer")[0];
-  container.innerHTML = "";
-}
+  function clearAppContainer() {
+    let container = document.getElementsByClassName("appContainer")[0];
+    container.innerHTML = "";
+  }
 
 // HTML content to render Stockist Tool Bar
-function renderStockListHeader() {
-  return `
+  function renderStockListHeader() {
+    return `
     <header class="contentStrip">
         <h1>STOKR</h1>
         <div class="buttonContainer">
@@ -51,24 +48,24 @@ function renderStockListHeader() {
           <button class="headerButton buttonSettings"></button>
         </div>
     </header>`
-}
+  }
 
 // Render Specific Stock Item Line
-function renderStockListItem(data, buttonState) {
-  const { Name, Change, PercentChange, LastTradePriceOnly } = data;
+  function renderStockListItem(data, buttonState) {
+    const {Name, Change, PercentChange, LastTradePriceOnly} = data;
 
-  // round stockprice to two digits
-  const roundedLastTradePriceOnly = parseFloat(LastTradePriceOnly).toFixed(2);
+    // round stockprice to two digits
+    const roundedLastTradePriceOnly = parseFloat(LastTradePriceOnly).toFixed(2);
 
-  // round Change
-  const roundedChange = parseFloat(Change).toFixed(2);
+    // round Change
+    const roundedChange = parseFloat(Change).toFixed(2);
 
-  // add positive css class incase % is positive
-  const priceDirection = PercentChange.includes("-")? "" : "positive";
+    // add positive css class incase % is positive
+    const priceDirection = PercentChange.includes("-") ? "" : "positive";
 
-  const buttonContent = buttonState === "1" ? PercentChange : roundedChange;
+    const buttonContent = buttonState === "1" ? PercentChange : roundedChange;
 
-  return `
+    return `
     <li class="contentStrip stockItem">
       <span class="companyTag">${Name}</span>
       <div class="stockItemRight">
@@ -81,61 +78,63 @@ function renderStockListItem(data, buttonState) {
       </div>
     </li>
   `
-}
+  }
 
 //render stocklist section
-function renderStockList(data, buttonState) {
-  return data.map( (item) => {
-    return renderStockListItem(item, buttonState)
-  });
-}
+  function renderStockList(data, buttonState) {
+    return data.map((item) => {
+      return renderStockListItem(item, buttonState)
+    });
+  }
 
 //Render Stocklist Container with Stocklist data
-function renderStockListContainer (data, buttonState) {
-  const renderedHTML = renderStockList(data, buttonState).join("");
+  function renderStockListContainer(data, buttonState) {
+    const renderedHTML = renderStockList(data, buttonState).join("");
 
-  return `
+    return `
     <ul class="stockListContainer">
        ${renderedHTML}
     </ul>
   `
-}
-
-function placeStockList(stockData, buttonState) {
-  //clear all content from screen
-  clearAppContainer();
-
-  //datacontainer for all HTML to be rendered for stock list page
-  let dataToRender = [];
-
-  // render stocklist header
-  dataToRender.push(renderStockListHeader());
-
-  // render stocklist
-  dataToRender.push(renderStockListContainer(stockData, buttonState));
-
-  // append header to container
-  let container = document.getElementsByClassName("appContainer")[0];
-  container.innerHTML = dataToRender.join("");
-
-  //add event listener
-  document.getElementsByClassName("stockListContainer")[0].addEventListener("click", eventToggleButtonInfo);
-}
-
-function eventToggleButtonInfo(event) {
-  const target = event.target.className;
-
-  if(target.includes("stockButton ")) {
-    buttonState = buttonState === "1" ? "2" : "1";
-
-    //rerender page
-    placeStockList(buttonState);
   }
-}
 
-function init() {
-  //Initializes page with StockList
-  placeStockList(stockData, buttonState);
-}
+  function placeStockList(stockData, buttonState) {
+    //clear all content from screen
+    clearAppContainer();
 
-init();
+    //datacontainer for all HTML to be rendered for stock list page
+    let dataToRender = [];
+
+    // render stocklist header
+    dataToRender.push(renderStockListHeader());
+
+    // render stocklist
+    dataToRender.push(renderStockListContainer(stockData, buttonState));
+
+    // append header to container
+    let container = document.getElementsByClassName("appContainer")[0];
+    container.innerHTML = dataToRender.join("");
+
+    //add event listener
+    document.getElementsByClassName("stockListContainer")[0].addEventListener("click", eventToggleButtonInfo);
+  }
+
+  function eventToggleButtonInfo(event) {
+    const target = event.target.className;
+
+    if (target.includes("stockButton ")) {
+      buttonState = buttonState === "1" ? "2" : "1";
+
+      //rerender page
+      placeStockList(buttonState);
+    }
+  }
+
+  function init() {
+    //Initializes page with StockList
+    placeStockList(stockData, buttonState);
+  }
+
+  init();
+
+})();
